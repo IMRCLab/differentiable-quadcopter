Fixed gradient error and logic for qexp

quadrotor_pytorch.py
@@ -2,44 +2,66 @@ import torch
 
 # Quaternion routines adapted from rowan to use autograd
 def qmultiply(q1, q2):
-	t1 = torch.mul(q1[:,:1], q2[:,:1]) - torch.sum(torch.mul(q1[:,1:4],q2[:,1:4]), dim=1, keepdim=True)
-	t2 = q1[:,:1] * q2[:,1:4] + q2[:,:1] * q1[:,1:4] + torch.cross(q1[:,1:4], q2[:,1:4], dim=1)
-	return torch.cat((t1, t2), dim=1)
+	t1 = torch.mul(q1[...,:1], q2[...,:1]) - torch.sum(torch.mul(q1[...,1:4],q2[...,1:4]), dim=-1, keepdim=True)
+	t2 = q1[...,:1] * q2[...,1:4] + q2[...,:1] * q1[...,1:4] + torch.cross(q1[...,1:4], q2[...,1:4], dim=-1)
+	return torch.cat((t1, t2), dim=-1)
 
 def qconjugate(q):
-	return torch.cat((q[:,0:1],-q[:,1:4]), dim=1)
+	return torch.cat((q[...,0:1],-q[...,1:4]), dim=-1)
 
 def qrotate(q, v):
-	quat_v = torch.cat((torch.zeros((v.shape[0],1)), v), dim=1)
-	return qmultiply(q, qmultiply(quat_v, qconjugate(q)))[:,1:]
+	quat_v = torch.cat((torch.zeros((v.shape[0],1)), v), dim=-1)
+	return qmultiply(q, qmultiply(quat_v, qconjugate(q)))[...,1:]
 
 def qexp_regular_norm(q):
-	e = torch.exp(q[:,:1])
-	norm = torch.linalg.norm(q[:,1:4], dim=1, keepdim=True)
-	result_v = e * q[:,1:4] / norm * torch.sin(norm)
+	e = torch.exp(q[...,:1])
+	norm = torch.linalg.norm(q[...,1:4], dim=-1, keepdim=True)
+	result_v = e * q[...,1:4] / norm * torch.sin(norm)
 	result_w = e * torch.cos(norm)
-	return torch.cat((result_w, result_v), dim=1)
+	return torch.cat((result_w, result_v), dim=-1)
 
 def qexp_zero_norm(q):
-	e = torch.exp(q[:,:1])
+	e = torch.exp(q[...,:1])
 	result_v = torch.zeros((q.shape[0], 3))
 	result_w = e
-	return torch.cat((result_w, result_v), dim=1)
+	return torch.cat((result_w, result_v), dim=-1)
 
 
 def qexp(q):
-	if torch.allclose(q[:,1:4], torch.zeros_like(q[:,1:4])):
-		return qexp_zero_norm(q)
+	"""
+	Adopted from rowan
+	"""
+	expo = torch.empty_like(q)
+	norms = torch.linalg.norm(q[..., 1:], dim=-1)
+	e = torch.exp(q[..., 0])
+	expo[...,0] = e * torch.cos(norms)
+	norm_zero = torch.isclose(norms, torch.zeros_like(norms))
+	not_zero = torch.logical_not(norm_zero)
+	if torch.any(not_zero):
+		expo[not_zero,1:] = (
+			e[not_zero, torch.newaxis]
+			* (q[not_zero, 1:] / norms[not_zero, torch.newaxis])
+			* torch.sin(norms)[not_zero, torch.newaxis]
+		)
+		if torch.any(norm_zero):
+			expo[norm_zero, 1:] = 0
 	else:
-		return qexp_regular_norm(q)
-	# return torch.where(torch.allclose(q[1:4], torch.zeros_like(q[1:4])), qexp_zero_norm(q), qexp_regular_norm(q))
+		expo[..., 1:] = 0
+	
+	return expo
+	# if torch.allclose(q[...,1:4], torch.zeros_like(q[...,1:4])):
+	# 	return qexp_zero_norm(q)
+	# else:
+	# 	return qexp_regular_norm(q)
+	# gradient computation using where is not working properly
+	# return torch.where(torch.all(torch.isclose(q[...,1:4], torch.zeros_like(q[...,1:4])), dim=-1,keepdim=True), qexp_zero_norm(q), qexp_regular_norm(q))
 
 def qintegrate(q, v, dt):
-	quat_v = torch.cat((torch.zeros((v.shape[0],1)), v*dt/2),dim=1)
+	quat_v = torch.cat((torch.zeros((v.shape[0],1)), v*dt/2),dim=-1)
 	return qmultiply(qexp(quat_v), q)
 
 def qnormalize(q):
-	return q / torch.linalg.norm(q, dim=1, keepdim=True)
+	return q / torch.linalg.norm(q, dim=-1, keepdim=True)
 
 
 class QuadrotorAutograd():
@@ -63,7 +85,7 @@ class QuadrotorAutograd():
 		# 	[0.83,16.66,1.8],
 		# 	[0.72,1.8,29.26]
 		# 	]) * 1e-6  # kg m^2
-		self.J = torch.tensor([16.571710e-6, 16.655602e-6, 29.261652e-6])
+		self.J = torch.tensor([16.571710e-6, 16.655602e-6, 29.261652e-6], dtype=torch.float64)
 
 		# Note: we assume here that our control is forces
 		arm_length = 0.046 # m
@@ -87,22 +109,22 @@ class QuadrotorAutograd():
 
 	def step(self, state, force):
 		# compute next state
-		q = state[:,6:10]
-		omega = state[:,10:]
+		q = state[...,6:10]
+		omega = state[...,10:]
 
 		# eta = torch.mv(self.B0, force)
 		eta = force @ self.B0.T
 		# f_u = torch.tensor([0,0,eta[0]])
 		batch_size = state.shape[0]
-		f_u = torch.cat((torch.zeros((batch_size, 2)),eta[:,0:1]),dim=1)
+		f_u = torch.cat((torch.zeros((batch_size, 2)),eta[...,:1]),dim=-1)
 		# tau_u = torch.tensor([eta[1],eta[2],eta[3]])
-		tau_u = eta[:,1:]
+		tau_u = eta[...,1:]
 
 		# dynamics 
 		# dot{p} = v 
-		pos_next = state[:,0:3] + state[:,3:6] * self.dt
+		pos_next = state[...,:3] + state[...,3:6] * self.dt
 		# mv = mg + R f_u 
-		vel_next = state[:,3:6] + (torch.tensor([0,0,-self.g]) + qrotate(q,f_u) / self.mass) * self.dt
+		vel_next = state[...,3:6] + (torch.tensor([0,0,-self.g]) + qrotate(q,f_u) / self.mass) * self.dt
 
 		# dot{R} = R S(w)
 		# to integrate the dynamics, see
@@ -113,17 +135,17 @@ class QuadrotorAutograd():
 
 		# mJ = Jw x w + tau_u
 		inv_J = 1 / self.J  # diagonal matrix -> division
-		omega_next = state[:,10:] + (inv_J * (torch.cross(self.J * omega,omega) + tau_u)) * self.dt
+		omega_next = state[...,10:] + (inv_J * (torch.cross(self.J * omega,omega, dim=-1) + tau_u)) * self.dt
 
-		return torch.cat((pos_next, vel_next, q_next, omega_next), dim=1)
+		return torch.cat((pos_next, vel_next, q_next, omega_next), dim=-1)
 
 
 if __name__ == '__main__':
 
 	robot = QuadrotorAutograd()
 
-	xbar = torch.tensor([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], requires_grad=True, dtype=torch.float64)
-	ubar = torch.tensor([0, 0, 0, 0], requires_grad=True, dtype=torch.float64)
+	xbar = torch.tensor([[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]], requires_grad=True, dtype=torch.float64)
+	ubar = torch.tensor([[0, 0, 0, 0]], requires_grad=True, dtype=torch.float64)
 
 	print(torch.autograd.gradcheck(robot.step, (xbar, ubar)))
 

train_system_id.py
@@ -53,7 +53,7 @@ class QuadrotorModule(nn.Module):
 
 # quaternion norm (adopted from rowan)
 def qnorm(q):
-    return torch.linalg.norm(q, dim=1, keepdim=True)
+    return torch.linalg.norm(q, dim=-1, keepdim=True)
 
 # quaternion sym distance (adopted from rowan)
 def qsym_distance(p, q):
@@ -219,7 +219,7 @@ if __name__ == '__main__':
     dt, training_data = load(args.file_usd_train)
     dt2, test_data = load(args.file_usd_test)
 
-    train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+    train_dataloader = DataLoader(training_data, batch_size=128, shuffle=True)
     test_dataloader = DataLoader(test_data, batch_size=64)
 
 
@@ -228,7 +228,7 @@ if __name__ == '__main__':
     # loss_fn = nn.MSELoss()
     loss_fn = QuadrotorLoss()
 
-    learning_rate = 3e-2
+    learning_rate = 1e-3
     epochs = 10
 
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)